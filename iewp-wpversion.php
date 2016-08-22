0ef96725a4fa0b0f3431aa49f6ddfab76872de2a
Philip Newborough <corenominal@corenominal.org>
2016-08-22 08:30:54 +0100

    Required file

---
+++ iewp-wpversion.php
@@ -0,0 +1,10 @@
+<?php
+if ( ! defined( 'WPINC' ) ) { die('Direct access prohibited!'); }
+/**
+ * Plugin Name: IEWP WordPress Version
+ * Plugin URI: https://github.com/corenominal/iewp-wpversion
+ * Description: A simple WordPress plugin that returns the version number of WordPress.
+ * Author: Philip Newborough
+ * Version: 0.0.1
+ * Author URI: https://corenominal.org
+ */
